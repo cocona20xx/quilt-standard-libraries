6b276cc521d364490944b14e72ebc33a85628c1b
Cocona <pexium128@gmail.com>
2023-07-08 20:39:42 -0400

    Fix unused import in MultiPackResourceManagerMixin, ran :applyLicenses

--- library/core/resource_loader/src/main/java/org/quiltmc/qsl/resource/loader/mixin/MultiPackResourceManagerMixin.java
+++ library/core/resource_loader/src/main/java/org/quiltmc/qsl/resource/loader/mixin/MultiPackResourceManagerMixin.java
@@ -29,7 +29,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
